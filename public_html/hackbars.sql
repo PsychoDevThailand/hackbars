1131111ae972e304b0712cf5310dd76c963bf000
Pathana Suphaphuditsophon <psuphaphuditsophon@gmail.com>
2020-04-06 22:22:39 +0700

    add new sql

--- public_html/hackbars.sql
+++ public_html/hackbars.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Apr 01, 2020 at 04:17 AM
--- Server version: 10.3.22-MariaDB
--- PHP Version: 7.2.28
+-- Host: 127.0.0.1
+-- Generation Time: Apr 06, 2020 at 05:17 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `heretumc_bars`
+-- Database: `hackbars`
 --
 
 -- --------------------------------------------------------
@@ -612,6 +611,18 @@ INSERT INTO `formula` (`id`, `type`, `data`, `created`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `phones`
+--
+
+CREATE TABLE `phones` (
+  `id` int(11) NOT NULL,
+  `phone` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `otp` varchar(4) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_bac_log`
 --
 
@@ -693,7 +704,8 @@ CREATE TABLE `users` (
   `ip` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `login_log` text COLLATE utf8_unicode_ci DEFAULT NULL,
   `refill_log` text COLLATE utf8_unicode_ci DEFAULT NULL,
-  `created` timestamp NOT NULL DEFAULT current_timestamp()
+  `created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `join_mm88get` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ROW_FORMAT=COMPACT;
 
 --
@@ -719,6 +731,12 @@ ALTER TABLE `formula`
   ADD PRIMARY KEY (`id`) USING BTREE;
 
 --
+-- Indexes for table `phones`
+--
+ALTER TABLE `phones`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `tbl_bac_log`
 --
 ALTER TABLE `tbl_bac_log`
@@ -742,6 +760,7 @@ ALTER TABLE `tw_settings`
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`) USING BTREE,
   ADD UNIQUE KEY `uname` (`uname`) USING BTREE,
+  ADD UNIQUE KEY `phone` (`phone`),
   ADD KEY `id` (`id`),
   ADD KEY `id_2` (`id`);
 
@@ -766,6 +785,12 @@ ALTER TABLE `contact`
 --
 ALTER TABLE `formula`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=810;
+
+--
+-- AUTO_INCREMENT for table `phones`
+--
+ALTER TABLE `phones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tbl_bac_log`
