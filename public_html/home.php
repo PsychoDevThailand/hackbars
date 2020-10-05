8b10207abd4396f952dad9fc1275b35b23f0ba42
Pathana Suphaphuditsophon <psuphaphuditsophon@gmail.com>
2020-10-06 00:07:09 +0700

    change banner football

--- public_html/home.php
+++ public_html/home.php
@@ -246,7 +246,7 @@ $asset_path = "asset/".$_SESSION['FormulaType'];
             <?php endif; ?>
           >
             <div class="game_div">
-              <img src="resource/images/cas/Game-football-new.png" style="width: 100%;">
+              <img src="resource/images/cas/Game-football-new.jpg" style="width: 100%;">
             </div>
           </a>
         </div>
