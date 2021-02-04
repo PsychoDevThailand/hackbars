7092feecc65ee41bf6fefc25a38e651ff6166db3
pathana <pathana@pathanas-iMac.local>
2021-02-04 23:22:47 +0700

    update env

--- public_html/env.php
+++ public_html/env.php
@@ -3,7 +3,7 @@
     'DB_HOST' => 'localhost',
     'DB_USERNAME' => 'root',
     'DB_PASSWORD' => '',
-    'DB_NAME' => 'spy777_db',
+    'DB_NAME' => 'formula_db',
     'DB_PORT' => '3306',
     'SMS_USERNAME' => '0968745703',
     'SMS_PASSWORD' => '129591',
